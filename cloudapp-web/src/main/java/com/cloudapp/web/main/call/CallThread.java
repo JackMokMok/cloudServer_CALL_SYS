b7ed66378e73847ffc938810e3180a65c71b7766
Jack <mosaijifly@163.com>
2022-01-05 23:58:41 +0800

    1）hot-fix分支的呼叫系统代码第2次提交。

--- cloudapp-web/src/main/java/com/cloudapp/web/main/call/CallThread.java
+++ cloudapp-web/src/main/java/com/cloudapp/web/main/call/CallThread.java
@@ -82,6 +82,7 @@ public class CallThread extends Thread {
             logger.warn("Socket启动...");
             logger.warn("Socket启动2...");
             logger.warn("Socket启动3...");
+            logger.warn("hot-fix Socket启动3...");
             serverSocket = new ServerSocket(10086);
             while (true){
                 final Socket socket = serverSocket.accept();
